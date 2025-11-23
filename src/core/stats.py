d03adac3debb6a5ef6215c74855651b9344d9876
Robin Langlois <robin.langlois@es.fcm.travel>
2025-11-23 13:40:35 +0100

    add basic __repr__ & __str__

--- src/core/stats.py
+++ src/core/stats.py
@@ -1,19 +1,26 @@
 class Stats:
-    def __init__(self, name, hp:int, attack:int, defense:int, magic:int, luck:int, speed:int):
-        name    = self.name
+    def __init__(self,  hp:int, attack:int, defense:int, magic:int, luck:int, speed:int, range:int):
         hp      = self.hp
         attack  = self.attack
         defense = self.defense
         magic   = self.magic
         luck    = self.luck
         speed   = self.speed
+        range   = self.range
+
+    def __repr__(self):
+        class_name = type(self).__name__
+
+        return f"{class_name}(hp={self.hp!r},attack={self.attack!r},defense={self.defense!r},magic={self.magic!r})"
 
     def __str__(self):
         return f""" Hp      : {self.hp}
                     Attack  : {self.attack}
                     Defense : {self.defense}
                     Magic   : {self.magic}
-                    Luck   : {self.luck}    
+                    Luck    : {self.luck}
+                    Speed   : {self.speed}
+                    Range   : {self.range}        
                 """
 
     def is_alive(self) -> bool:
