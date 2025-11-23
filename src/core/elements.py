0443c5cadb45bac413b6424503726219223c533c
Binardino <langlois.robin@gmail.com>
2025-11-23 14:17:43 +0100

    initial commit elements.py with basic 19 elements

---
+++ src/core/elements.py
@@ -0,0 +1,22 @@
+from enum import Enum, auto
+
+class Element(Enum):
+    FIRE    = auto() # 火 - 灬
+    WATER   = auto() #水 - 氵
+    ICE     = auto()   #氷 - 冫
+    WIND    = auto() #風 - 气
+    EARTH   = auto() #土 - 石
+    STONE   = auto() #石
+    PLANT   = auto() #艹
+    TREE    = auto() #木 - 竹
+    METAL   = auto() #金
+    THUNDER = auto() #雷
+    LIGHT   = auto() #光
+    DARK    = auto() #玄
+    SUN     = auto() #日
+    MOON    = auto() #月
+    DRAGON  = auto() #龍
+    INSECT  = auto() #虫
+    POISON  = auto() # 疒 - 毒
+    VOID    = auto() #空
+    SPIRIT  = auto() #心
