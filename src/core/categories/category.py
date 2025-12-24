cbb6b3c88f1ea6f2248b2421b55c65e6b638e706
Binardino <langlois.robin@gmail.com>
2025-12-24 10:15:49 +0100

    initial commit category.py

---
+++ src/core/categories/category.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+class Category(Enum):
+    ANIMAL   = auto()
+    HUMAN    = auto()
+    PLANT    = auto()
+    SPIRIT   = auto()
+    WEATHER  = auto()
+    BODY     = auto()
+    TOOL     = auto()
+    MATERIAL = auto()
+    NATURE   = auto()
+    SOCIETY  = auto()
